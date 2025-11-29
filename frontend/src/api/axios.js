94c04e2d0df4f05f9d36d353c8c03e65a8dfbee4
Avala vinay <avalavinay4@gmail.com>
2025-11-29 19:30:02 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://placement-prep-9xgx.onrender.com/api/",
 });
 
 export default API;
