4fc67d9c5b702be9b6ecf2be45579bc144f28b8b
Avala vinay <avalavinay4@gmail.com>
2025-12-24 23:50:50 +0530

    Update Problem.js

--- backend/models/Problem.js
+++ backend/models/Problem.js
@@ -1,21 +1,47 @@
 const mongoose = require("mongoose");
 
-const problemSchema = new mongoose.Schema({
-  contestId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Contest",
-    required: true,
-  },
-  title: String,
-  description: String,
-  functionName: String,
-  testCases: [
-    {
-      input: mongoose.Schema.Types.Mixed,
-      output: mongoose.Schema.Types.Mixed,
+const problemSchema = new mongoose.Schema(
+  {
+    contestId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Contest",
+      required: true,
     },
-  ],
-  score: Number,
-});
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    functionName: {
+      type: String,
+      required: true,
+    },
+
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      default: "easy",
+    },
+
+    testCases: [
+      {
+        input: mongoose.Schema.Types.Mixed,
+        output: mongoose.Schema.Types.Mixed,
+      },
+    ],
+
+    score: {
+      type: Number,
+      default: 100,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Problem", problemSchema);
