4bd5e816c7f42b7554f2c8bbab44d9f8c51cb390
Avala vinay <avalavinay4@gmail.com>
2025-12-24 21:14:42 +0530

    Create Problem.js

---
+++ backend/models/Problem.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const problemSchema = new mongoose.Schema({
+  contestId: { type: mongoose.Schema.Types.ObjectId, ref: "Contest" },
+  title: String,
+  description: String,
+  functionName: String,
+  testCases: [
+    {
+      input: mongoose.Schema.Types.Mixed,
+      output: mongoose.Schema.Types.Mixed,
+    },
+  ],
+  score: Number,
+});
+
+module.exports = mongoose.model("Problem", problemSchema);
