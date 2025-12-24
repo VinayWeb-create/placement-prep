ff33fda8fecd1089dffc5dd9b34673065b3cf663
Avala vinay <avalavinay4@gmail.com>
2025-12-25 00:05:22 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,9 +11,9 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // { id, iat, exp }
+    req.user = decoded; // { id, email, role }
     next();
-  } catch (err) {
+  } catch {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
