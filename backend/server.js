83c4e996fa87f7168bc8010841c9a8537bd4efba
Avala vinay <avalavinay4@gmail.com>
2025-12-24 21:25:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(express.json());
 
 /* ---------- ROUTES ---------- */
 app.use("/api/auth", authRoutes);
+app.use("/api/contests", require("./routes/contestRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully 🚀");
