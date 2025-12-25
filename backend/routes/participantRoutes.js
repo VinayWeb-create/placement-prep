d06b601301d3e1a143c13600183f5f98c9f3eab5
Avala vinay <avalavinay4@gmail.com>
2025-12-25 15:52:36 +0530

    Change Participant model import to Registration

--- backend/routes/participantRoutes.js
+++ backend/routes/participantRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Participant = require("../models/Participant");
+const Participant = require("../models/Registration");
 const auth = require("../middleware/auth");
 
 // REGISTER FOR CONTEST
