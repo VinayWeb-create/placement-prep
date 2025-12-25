e3029390c880006c638ec0b1b93b0077f5408806
Avala vinay <avalavinay4@gmail.com>
2025-12-25 19:18:11 +0530

    Remove unused API import in CodingEditor.jsx
    Removed unused API import from CodingEditor component.

--- frontend/src/pages/CodingEditor.jsx
+++ frontend/src/pages/CodingEditor.jsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import API from "../../api/axios";
 
 export default function CodingEditor() {
   const { contestId } = useParams();
